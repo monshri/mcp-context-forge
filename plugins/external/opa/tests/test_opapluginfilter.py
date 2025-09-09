2a0fdd635c91fc980f6382c2957d17558c0a958c
Shriti Priya <shritip@ibm.com>
2025-09-09 16:01:58 -0400

    test cases for opapluginfilter
    Signed-off-by: Shriti Priya <shritip@ibm.com>

--- plugins/external/opa/tests/test_opapluginfilter.py
+++ plugins/external/opa/tests/test_opapluginfilter.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
-"""Tests for plugin."""
+"""Test cases for OPA plugin
+
+Copyright 2025
+SPDX-License-Identifier: Apache-2.0
+Authors: Shriti Priya
+
+This module contains test cases for running opa plugin.
+"""
+
 
 # Third-Party
 import pytest
@@ -13,23 +21,71 @@ from mcpgateway.plugins.framework import (
     GlobalContext
 )
 
+from tests.server.opa_server import run_mock_opa
+
 
 @pytest.mark.asyncio
-async def test_opapluginfilter():
+# Test for when opaplugin is not applied to a tool
+async def test_benign_opapluginfilter():
     """Test plugin prompt prefetch hook."""
     config = PluginConfig(
         name="test",
         kind="opapluginfilter.OPAPluginFilter",
         hooks=["tool_pre_invoke"],
-        config={"setting_one": "test_value"},
+        applied_to = {"tools" : [{"tool_name": "fast-time-git-status", "context": ["global.opa_policy_context.git_context"], "extensions": {"policy": "example", "policy_endpoint" : "allow"}}]},
+        config={"opa_base_url": "http://127.0.0.1:8181/v1/data/"}
     )
+    mock_server = run_mock_opa()
+    
 
     plugin = OPAPluginFilter(config)
 
     # Test your plugin logic
-    payload = ToolPreInvokePayload(name="test_tool", args={"repo_path": "This is an argument"})
+    payload = ToolPreInvokePayload(name="fast-time-git-status", args={"repo_path": "/path/IBM"})
     context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2"))
     result = await plugin.tool_pre_invoke(payload, context)
-    import pdb
-    pdb.set_trace()
+    mock_server.shutdown()
+    assert result.continue_processing
+
+
+@pytest.mark.asyncio
+# Test for when opaplugin is not applied to a tool
+async def test_malign_opapluginfilter():
+    """Test plugin prompt prefetch hook."""
+    config = PluginConfig(
+        name="test",
+        kind="opapluginfilter.OPAPluginFilter",
+        hooks=["tool_pre_invoke"],
+        applied_to = {"tools" : [{"tool_name": "fast-time-git-status", "context": ["global.opa_policy_context.git_context"], "extensions": {"policy": "example", "policy_endpoint" : "allow"}}]},
+        config={"opa_base_url": "http://127.0.0.1:8181/v1/data/"}
+    )
+    mock_server = run_mock_opa()
+    plugin = OPAPluginFilter(config)
+
+    # Test your plugin logic
+    payload = ToolPreInvokePayload(name="fast-time-git-status", args={"repo_path": "/path/IM"})
+    context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2"))
+    result = await plugin.tool_pre_invoke(payload, context)
+    mock_server.shutdown()
+    assert not result.continue_processing and result.violation.code == "deny"
+    
+@pytest.mark.asyncio
+# Test for opa plugin not applied to any of the tools
+async def test_applied_to_opaplugin():
+    """Test plugin prompt prefetch hook."""
+    config = PluginConfig(
+        name="test",
+        kind="opapluginfilter.OPAPluginFilter",
+        hooks=["tool_pre_invoke"],
+        applied_to = {},
+        config={"opa_base_url": "http://127.0.0.1:8181/v1/data/"}
+    )
+    mock_server = run_mock_opa()
+    plugin = OPAPluginFilter(config)
+
+    # Test your plugin logic
+    payload = ToolPreInvokePayload(name="fast-time-git-status", args={"repo_path": "/path/IM"})
+    context = PluginContext(global_context=GlobalContext(request_id="1", server_id="2"))
+    result = await plugin.tool_pre_invoke(payload, context)
+    mock_server.shutdown()
     assert result.continue_processing
